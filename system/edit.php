048824bce5900ea8ea8e8394134fa9a3b1e78d9c
unknown <imthedank69@gmail.com>
2023-09-15 19:39:18 +0700

    adding edit function

--- system/edit.php
+++ system/edit.php
@@ -1,11 +1,17 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>edit</title>
-</head>
-<body>
-	
-</body>
-</html>
+<?php 
+
+	include 'connect.php';
+
+	$id = $_GET['id'];
+    $nama_dpn = $_REQUEST['nama_depan'];
+    $nama_blkg = $_REQUEST['nama_belakang'];
+    $email = $_REQUEST['email'];
+    $password = $_REQUEST['password'];
+
+    $sql = "UPDATE user set nama_depan = '$nama_dpn', nama_belakang = '$nama_blkg', email = '$email', password = '$password' where id = $id";
+
+    header('location: ../view_data.php');
+
+    mysqli_query($conn, $sql)
+
+ ?>
